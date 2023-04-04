f6047277dddb759e8c70a2ed87742a60d3e9857c
Joshua Perrett <jperrett256@gmail.com>
2023-04-04 06:22:21 +0100

    cleaned up, logging mode switches and exceptions

--- accel/tcg/log_instr_drcachesim.c
+++ accel/tcg/log_instr_drcachesim.c
@@ -38,21 +38,6 @@ void init_drcachesim_backend(CPUArchState * env)
     // TODO could switch to w+b file mode, but then should have a header in trace to separate runs?
 }
 
-// NOTE copied from qemu/softmmu/memory.c
-static hwaddr memory_region_to_absolute_addr(MemoryRegion *mr, hwaddr offset)
-{
-    MemoryRegion *root;
-    hwaddr abs_addr = offset;
-
-    abs_addr += mr->addr;
-    for (root = mr; root->container; ) {
-        root = root->container;
-        abs_addr += root->addr;
-    }
-
-    return abs_addr;
-}
-
 void emit_drcachesim_entry(CPUArchState * env, cpu_log_entry_t * entry)
 {
     assert(output_trace_file);
@@ -62,18 +47,19 @@ void emit_drcachesim_entry(CPUArchState * env, cpu_log_entry_t * entry)
     {
         target_ulong pc = entry->pc;
 
-        MemTxAttrs attrs_instr;
-        hwaddr instr_haddr = cpu_get_phys_page_attrs_debug(env_cpu(env), pc & TARGET_PAGE_MASK, &attrs_instr);
-        if (instr_haddr != -1) instr_haddr += pc & ~TARGET_PAGE_MASK;
+        MemTxAttrs instr_attrs;
+        hwaddr instr_paddr = cpu_get_phys_page_attrs_debug(env_cpu(env), pc & TARGET_PAGE_MASK, &instr_attrs);
+        if (instr_paddr != -1) instr_paddr += pc & ~TARGET_PAGE_MASK;
 
-        fprintf(output_dbg_file, "Instruction [ pc: " TARGET_FMT_lx ", hwaddr: %" HWADDR_PRIx ", opcode: ", pc, instr_haddr);
+        fprintf(output_dbg_file, "Instruction [ pc: " TARGET_FMT_lx ", paddr: %" HWADDR_PRIx ", opcode: ", pc, instr_paddr);
         for (int64_t i = 0; i < entry->insn_size; i++)
         {
             fprintf(output_dbg_file, "%02hhx", (uint8_t) entry->insn_bytes[i]);
         }
         fprintf(output_dbg_file, " ]\n");
 
-        // TODO check not exception/trap?
+        if (entry->flags & LI_FLAG_MODE_SWITCH) fprintf(output_dbg_file, "CPU MODE SWITCH\n");
+        if (entry->flags & LI_FLAG_INTR_MASK) fprintf(output_dbg_file, "EXCEPTION\n");
 
         for (int64_t i = 0; i < entry->mem->len; i++)
         {
@@ -101,50 +87,13 @@ void emit_drcachesim_entry(CPUArchState * env, cpu_log_entry_t * entry)
             uint32_t size = memop_size(minfo->op);
             target_ulong vaddr = minfo->addr;
 
-            int mmu_idx = cpu_mmu_index(env, false);
-            void * host_addr = NULL;
-
-            if (minfo->flags & LMI_LD)
-            {
-                host_addr = probe_read(env, vaddr, 1, mmu_idx, pc);
-            }
-            else if (minfo->flags & LMI_ST)
-            {
-                if (minfo->flags & LMI_CAP)
-                {
-#ifdef TARGET_CHERI
-                    host_addr = probe_cap_write(env, vaddr, 1, mmu_idx, pc);
-#endif
-                }
-                else
-                {
-                    host_addr = probe_write(env, vaddr, 1, mmu_idx, pc); // TODO sometimes fails and returns NULL?
-                }
-            }
-            else
-                assert(false && "Memory access is neither load or store.");
-
-            ram_addr_t paddr = host_addr ? qemu_ram_addr_from_host(host_addr) : 0;
-
-            // TODO is the hwaddr what we actually want (vs ram addr)? why are the values >2GiB (memory of VM)?
-            hwaddr mr_haddr = 0;
-            hwaddr haddr = 0;
-            if (host_addr)
-            {
-                ram_addr_t offset;
-                MemoryRegion * mr = memory_region_from_host(host_addr, &offset);
-                haddr = memory_region_to_absolute_addr(mr, offset);
-                mr_haddr = mr->addr;
-            }
-
             MemTxAttrs attrs;
-            hwaddr haddr2 = cpu_get_phys_page_attrs_debug(env_cpu(env), vaddr & TARGET_PAGE_MASK, &attrs);
-            if (haddr2 != -1) haddr2 += vaddr & ~TARGET_PAGE_MASK;
+            hwaddr paddr = cpu_get_phys_page_attrs_debug(env_cpu(env), vaddr & TARGET_PAGE_MASK, &attrs);
+            if (paddr != -1) paddr += vaddr & ~TARGET_PAGE_MASK;
 
             fprintf(output_dbg_file,
-                "Memory Access [ type: %s, size: %u, \n"
-                "\tvaddr: " TARGET_FMT_lx ", ram addr: " RAM_ADDR_FMT ", mem region hwaddr: %" HWADDR_PRIx ", hwaddr: %" HWADDR_PRIx ", hwaddr2: %" HWADDR_PRIx " ]\n",
-                op_type_str, size, vaddr, paddr, mr_haddr, haddr, haddr2);
+                "Memory Access [ type: %s, size: %u, vaddr: " TARGET_FMT_lx ", paddr: %" HWADDR_PRIx " ]\n",
+                op_type_str, size, vaddr, paddr);
         }
     }
 }
